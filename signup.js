1b8ca62d8630e80d4c68489fbdfd32c04b7a929d
Kenny David <jp3193666@gmail.com>
2025-02-17 19:41:08 +0100

    Atualização do backend

--- signup.js
+++ signup.js
@@ -1,16 +1,23 @@
-let nomeDigitado = document.getElementById("nome").value;
-fetch('https://backend-eng9.onrender.com/register', { 
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-        name: nomeDigitado,
-        email: emailDigitado,
-        password: senhaDigitada
+function cadastrar() {
+    let nomeDigitado = document.getElementById("nome").value;
+    let emailDigitado = document.getElementById("email").value;
+    let senhaDigitada = document.getElementById("senha").value;
+
+    fetch('https://backend-en99.onrender.com/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name: nomeDigitado,
+            email: emailDigitado,
+            password: senhaDigitada
+        })
     })
-})
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error('Erro:', error));
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Erro:', error));
+}
+
+// Função para voltar à página inicial
 function voltar() {
     window.location.href = "index.html"; // Ajusta o caminho conforme necessário
 }
